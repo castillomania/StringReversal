f07a9fb70468c807f183518e97045f4f6b661723
David Castillo <david@castillomania.com>
2017-08-28 20:47:40 -0700

    Updated some comments and added clarity

--- StringTestApp/StringTest.cs
+++ StringTestApp/StringTest.cs
@@ -6,55 +6,64 @@ using System.Threading.Tasks;
 
 namespace StringTestApp
 {
+    /// <summary>
+    /// Notes: 
+    /// I have no doubt that this could be further optimized. In a real-world situation, I would probably have come up with this solution below, checked whether it met our scalability needs, and then started searching online for more efficient algorithms
+    /// 
+    /// I used a dictionary to store the letter counts because C# doesn't have a native HashTable. Using Java, I would have used that instead
+    /// </summary>
     public class StringTest
     {
         private Dictionary<string, long> _letterCount { get; set; }
 
+        /// <summary>
+        /// Constructor to initialize the dictionary
+        /// </summary>
         public StringTest()
         {
-            // initialize the dictionary in the constructor
             _letterCount = new Dictionary<string, long>(); 
         }
 
         /// <summary>
-        /// Notes;
-        /// I used a dictionary to store the letter counts because C# doesn't have a native HashTable. Using Java, I would have used that instead
+        /// This is the method that actually coordinates returning the desired output
         /// </summary>
-        /// <param name="input">The input string that needs to be reversed</param>
-        /// <param name="reversed">The reversed form of the input string</param>
+        /// <param name="inputString">The input string that needs to be reversed</param>
+        /// <param name="reversedString">The reversed form of the input string</param>
         /// <param name="letterCount">A dictionary containing each letter in the reversed string as well as the number of times the letter appears</param>
-        public void StringParser(string input, out string reversedString, out Dictionary<string, long> letterCount)
+        public void StringParser(string inputString, out string reversedString, out Dictionary<string, long> letterCount)
         {
-            reversedString = ReverseString(input); // this does the reversing and logging
+            reversedString = ReverseString(inputString); // this does the reversing and logging
             letterCount = _letterCount; // set the output variable to the member variable in which the character logging was stored
         }
 
         /// <summary>
         /// Reverses the provided string and counts the appearance of each character in the string
+        /// 
+        /// Assumption:
+        /// I inferred that non-letters were to be omitted from the logging. Thus, spaces are not counted
         /// </summary>
         /// <param name="input">The string to be reversed and logged</param>
         /// <returns></returns>
-        public string ReverseString(string input)
+        private string ReverseString(string inputString)
         {
-            char[] inputArray = input.ToCharArray(); // convert the string to a character array
+            char[] inputArray = inputString.ToCharArray(); // convert the string to a character array
 
-            long j = 0; // output array incrementer
+            long j = -1; // output array incrementer, set to -1 so that it increments properly the first time
             long i = inputArray.LongLength; // input array incrementer
             char[] outputArray = new char[i]; // container for the output
 
             // loop through each character on the input string, backwards
             while (i > 0)
             {
-                i--; // decrement the incrementer since C# uses 0-base array indexing
-                outputArray[j] = inputArray[i]; // add the decrementing char to the incrementing output
-
-                // only log the character if it is a letter (numbers and special characters aren't logged)
-                if (Char.IsLetter(inputArray[i]))
-                { 
-                    countLetters(inputArray[i].ToString()); // ensure that the added letter is counted
-                }
-
                 j++; // increment the output array for the next letter
+                i--; // decrement the incrementer since C# uses 0-base array indexing
+
+                outputArray[j] = inputArray[i]; // add the decrementing char to the incrementing output
+                
+                if (Char.IsLetter(inputArray[i])) // only log the character if it is a letter (numbers and special characters aren't logged)
+                { 
+                    CountLetters(inputArray[i].ToString()); // ensure that the added letter is counted
+                }
             }
 
             // return a new string created from the char array created above
@@ -69,7 +78,7 @@ namespace StringTestApp
         /// Non-letter characters should not be counted
         /// </summary>
         /// <param name="letter">The letter to be logged</param>
-        public void countLetters(string letter)
+        private void CountLetters(string letter)
         {
             // checks the dictionary to see if the letter has already been added
             if (_letterCount.ContainsKey(letter))
@@ -80,7 +89,7 @@ namespace StringTestApp
             }
             else
             {
-                // if the letter isn't already added, add the letter to the dictionary with a value of 1 to indicate a single instance of the letter
+                // if the letter isn't already added, add the letter to the dictionary with a value of 1 to indicate the first instance of the letter
                 _letterCount.Add(letter,1);
             }
         }
--- StringTestTests/StringTestTest.cs
+++ StringTestTests/StringTestTest.cs
@@ -6,54 +6,65 @@ using StringTestApp;
 namespace StringTestTests
 {
     /// <summary>
-    /// I only wrote happy-path tests because I didn't want to sink too much time into this exercise. Otherwise, I might have fleshed out a bunch of expected-failure tests as well
+    /// I'm sure we could come up with more tests, but this seems like a good first-pass
     /// </summary>
     [TestClass]
     public class StringTestTest
     {
+        private string _testString;
+        private string _testOutput;
+        private Dictionary<string, long> _letterCount;
+
+        [TestInitialize]
+        public void StringTestTestSetup()
+        {
+            _testString = "Seattle Pacific University";
+            _testOutput = "";
+            _letterCount = new Dictionary<string, long>();
+
+            StringTest stringTest = new StringTest();
+            stringTest.StringParser(_testString, out _testOutput, out _letterCount);
+        }
+
         [TestMethod]
         public void TestSuccessfulReversing()
         {
-            string testString = "Seattle Pacific University";
-            string testOutput = "";
-            Dictionary<string, long> letterCount = new Dictionary<string, long>();
-
-            StringTest stringTest = new StringTest();
-            stringTest.StringParser(testString, out testOutput, out letterCount);
-            
-            Assert.IsTrue(testOutput == "ytisrevinU cificaP elttaeS");
+            // makes sure that the output is properly reversed
+            Assert.IsTrue(_testOutput == "ytisrevinU cificaP elttaeS");
         }
 
         [TestMethod]
         public void TestSuccessfulLetterCount()
         {
-            string testString = "Seattle Pacific University";
-            string testOutput = "";
-            Dictionary<string, long> letterCount = new Dictionary<string, long>();
+            // there are 15 distinct characters in the test string
+            Assert.IsTrue(_letterCount.Count == 15); 
+        }
 
-            StringTest stringTest = new StringTest();
-            stringTest.StringParser(testString, out testOutput, out letterCount);
-
-            Assert.IsTrue(letterCount.Count == 15); // there are 15 distinct character in the test string
-
+        [TestMethod]
+        public void TestSuccessfulLoggingOfEachLetter()
+        {
             // validate each letter's count
-            Assert.IsTrue(letterCount.ContainsKey("S") && letterCount["S"] == 1);
-            Assert.IsTrue(letterCount.ContainsKey("e") && letterCount["e"] == 3);
-            Assert.IsTrue(letterCount.ContainsKey("a") && letterCount["a"] == 2);
-            Assert.IsTrue(letterCount.ContainsKey("t") && letterCount["t"] == 3);
-            Assert.IsTrue(letterCount.ContainsKey("l") && letterCount["l"] == 1);
-            Assert.IsTrue(letterCount.ContainsKey("P") && letterCount["P"] == 1);
-            Assert.IsTrue(letterCount.ContainsKey("c") && letterCount["c"] == 2);
-            Assert.IsTrue(letterCount.ContainsKey("i") && letterCount["i"] == 4);
-            Assert.IsTrue(letterCount.ContainsKey("f") && letterCount["f"] == 1);
-            Assert.IsTrue(letterCount.ContainsKey("U") && letterCount["U"] == 1);
-            Assert.IsTrue(letterCount.ContainsKey("n") && letterCount["n"] == 1);
-            Assert.IsTrue(letterCount.ContainsKey("v") && letterCount["v"] == 1);
-            Assert.IsTrue(letterCount.ContainsKey("r") && letterCount["r"] == 1);
-            Assert.IsTrue(letterCount.ContainsKey("y") && letterCount["y"] == 1);
+            Assert.IsTrue(_letterCount.ContainsKey("S") && _letterCount["S"] == 1);
+            Assert.IsTrue(_letterCount.ContainsKey("e") && _letterCount["e"] == 3);
+            Assert.IsTrue(_letterCount.ContainsKey("a") && _letterCount["a"] == 2);
+            Assert.IsTrue(_letterCount.ContainsKey("t") && _letterCount["t"] == 3);
+            Assert.IsTrue(_letterCount.ContainsKey("l") && _letterCount["l"] == 1);
+            Assert.IsTrue(_letterCount.ContainsKey("P") && _letterCount["P"] == 1);
+            Assert.IsTrue(_letterCount.ContainsKey("c") && _letterCount["c"] == 2);
+            Assert.IsTrue(_letterCount.ContainsKey("i") && _letterCount["i"] == 4);
+            Assert.IsTrue(_letterCount.ContainsKey("f") && _letterCount["f"] == 1);
+            Assert.IsTrue(_letterCount.ContainsKey("U") && _letterCount["U"] == 1);
+            Assert.IsTrue(_letterCount.ContainsKey("n") && _letterCount["n"] == 1);
+            Assert.IsTrue(_letterCount.ContainsKey("v") && _letterCount["v"] == 1);
+            Assert.IsTrue(_letterCount.ContainsKey("r") && _letterCount["r"] == 1);
+            Assert.IsTrue(_letterCount.ContainsKey("y") && _letterCount["y"] == 1);
+        }
 
+        [TestMethod]
+        public void TestThatSpacesAreExcluded()
+        {
             // ensures the space didn't get logged
-            Assert.IsFalse(letterCount.ContainsKey(" "));
+            Assert.IsFalse(_letterCount.ContainsKey(" "));
         }
     }
 }
